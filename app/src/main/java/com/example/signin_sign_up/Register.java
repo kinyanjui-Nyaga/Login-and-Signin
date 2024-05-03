2e0de5cdc90805267ed076ccdfbdc6a6d3a3fe4b
Kinyanjui-Nyaga <nyagakinyanjui@gmail.com>
2024-05-03 09:04:35 +0300

    Finished working on the registration part...

--- app/src/main/java/com/example/signin_sign_up/Register.java
+++ app/src/main/java/com/example/signin_sign_up/Register.java
@@ -1,14 +1,76 @@
 package com.example.signin_sign_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonreg;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonreg = findViewById(R.id.btn_register);
+        auth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        buttonreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText( Register.this, "Enter email...", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText( Register.this, "Enter password...", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                               if (task.isSuccessful()){
+                                   Toast.makeText(Register.this, "Account created.", Toast.LENGTH_SHORT).show();
+
+                               }else {
+                                   Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                               }
+                            }
+                        });
+
+            }
+        });
     }
+
 }
